e8cee41c98f3a5161382eba0938ee23132dd8975
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2022-10-30 09:28:21 +0100

    Add the hashing of password before saving to the DB

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     first_name: {
@@ -21,6 +22,12 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Please provide a password'],
         minlength: 8,
     }
+});
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
