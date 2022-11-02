ce6a4f6d26eeb290fd211092b107ff76d63b2c18
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2022-11-02 06:50:02 +0100

    Added a global error handling middleware

--- src/app.js
+++ src/app.js
@@ -10,10 +10,19 @@ app.use('/api/v1/user', userRouter);
 app.use('/api/v1/blogs', articleRouter);
 
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `The route ${req.originalUrl} does not exist! 💨`
-    })
+    const err = new Error(`The route ${req.originalUrl} does not exist! 💨`);
+    err.status = 'fail';
+    err.statusCode = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error! 💥';
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
 });
 
 module.exports = app;
