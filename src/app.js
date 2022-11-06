651cea91e4875e898f4dfa3166b9fd5fd22ab260
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2022-11-06 07:46:21 +0100

    Changed the home page to a html page

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const userRouter = require('./routes/userRoute');
-const articleRouter = require('./routes/articleRoute');
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
-const path = require('path');
-const compression = require('compression');
+const express = require("express");
+const userRouter = require("./routes/userRoute");
+const articleRouter = require("./routes/articleRoute");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+const path = require("path");
+const compression = require("compression");
 
 const app = express();
 
@@ -12,15 +12,15 @@ app.use(express.json());
 
 app.use(compression());
 
-app.use('/api/v1/user', userRouter);
-app.use('/api/v1/blogs', articleRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/blogs", articleRouter);
 
-app.use('/', function(req, res, next) {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+app.use("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-app.all('*', (req, res, next) => {
-    next(new AppError(`The route ${req.originalUrl} does not exist! 💨`, 404));
+app.all("*", (req, res, next) => {
+  next(new AppError(`The route ${req.originalUrl} does not exist! 💨`, 404));
 });
 
 app.use(globalErrorHandler);
